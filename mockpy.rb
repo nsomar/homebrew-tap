8738c84ffd80cc1fc110a39325cf4709d5c5f1b0
Omar Abdelhafith <omar.abdelhafith@mttnow.com>
2015-06-29 16:08:35 +0100

    Adding mockpy formula

---
+++ mockpy.rb
@@ -0,0 +1,15 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Mockpy < Formula
+  desc ""
+  homepage "https://github.com/oarrabi/mockpy"
+  url "https://github.com/oarrabi/mockpy/raw/master/releases/0.1.4/mockpy.zip"
+  version "0.1.4"
+  sha256 "2736c5608f3a166d8dd3055b60fbe438ae8a381b907b021156e7c8e05445a4ca"
+
+   def install
+    bin.install "mockpy"
+   end
+end
