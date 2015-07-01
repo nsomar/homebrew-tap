a8303e455f7f785e9eceab148556bdef91f109cc
Omar Abdelhafith <omar.abdelhafith@mttnow.com>
2015-07-01 01:33:24 +0100

    Removing homepage from formula

--- mockpy.rb
+++ mockpy.rb
@@ -3,7 +3,6 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class Mockpy < Formula
-  homepage "https://github.com/oarrabi/mockpy"
   url "https://github.com/oarrabi/mockpy/raw/master/releases/mockpy.zip"
   version "0.1.6"
   sha256 "b9f479699e4b503a7831242461f8d778e648c6d5d9909ccaaaa8411a9a96c698"
