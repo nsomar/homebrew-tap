a5c41dd4a0088219e1e0f62ce853978e7f37a731
Omar Abdelhafith <omar.abdelhafith@mttnow.com>
2015-07-01 01:09:02 +0100

    Adding mockpy version 0.1.5

--- mockpy.rb
+++ mockpy.rb
@@ -4,9 +4,9 @@
 
 class Mockpy < Formula
   homepage "https://github.com/oarrabi/mockpy"
-  url "https://github.com/oarrabi/mockpy/raw/master/releases/0.1.4/mockpy.zip"
-  version "0.1.4"
-  sha256 "2736c5608f3a166d8dd3055b60fbe438ae8a381b907b021156e7c8e05445a4ca"
+  url "https://github.com/oarrabi/mockpy/raw/master/releases/0.1.5/mockpy.zip"
+  version "0.1.5"
+  sha256 "bc70c7858afb290c6a022b6ddbfcab7be4b1cc83fbb96538013a9bd3900e3cc1"
 
    def install
     bin.install "mockpy"
