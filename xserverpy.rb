a1a6676be9cac9e987253b97949c229f45651efb
Omar Abdelhafith <omar.abdelhafith@mttnow.com>
2015-07-27 23:04:32 +0100

    Adding xserverpy version 0.2.2

--- xserverpy.rb
+++ xserverpy.rb
@@ -3,9 +3,9 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class Xserverpy < Formula
-  url "https://github.com/oarrabi/xserverpy/releases/download/0.2.1/xserverpy.zip"
-  version "0.2.1"
-  sha256 "9bf857f7f39272b4ef5106be65d67b1f0efb56d937c40f4d32e55d9ef3d6baa9"
+  url "https://github.com/oarrabi/xserverpy/releases/download/0.2.2/xserverpy.zip"
+  version "0.2.2"
+  sha256 "788244d26538e91df6208b2efe33bc8ad446225b430158a4c39383143eb7009a"
 
   def install
     bin.install "xserverpy"
