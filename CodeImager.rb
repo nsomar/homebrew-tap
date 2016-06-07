f7d699c0371aea2091670f20817ccab564537267
Omar Abdelhafith <o.arrabi@me.com>
2016-06-07 11:34:57 +0100

    Adding code imager

---
+++ CodeImager.rb
@@ -0,0 +1,7 @@
+cask 'CodeImager' do
+  version '0.1.0'
+  sha256 '8390a5cab83095334cda6e253e21c9eee0b812978d9f8e28715790d9122f7d5a'
+
+  url 'https://www.dropbox.com/s/37qfmnta0jp8lqd/CodeImager.dmg?dl=1'
+  name 'CodeImager'
+end
