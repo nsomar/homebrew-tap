fca19d0a66da4fa446996e629e756f694fd5f1a3
Omar Abdelhafith <o.arrabi@me.com>
2016-06-08 01:30:38 +0100

    adding version 0.1.1 of code imager

--- Casks/CodeImager.rb
+++ Casks/CodeImager.rb
@@ -1,7 +1,7 @@
 cask 'CodeImager' do
-  version '0.1.0'
-  sha256 '8390a5cab83095334cda6e253e21c9eee0b812978d9f8e28715790d9122f7d5a'
+  version '0.1.1'
+  sha256 'f80b537893eff07ddf5da95ac322a10296c7c13da300964b59ea1e54b3475ad1'
 
-  url 'https://www.dropbox.com/s/37qfmnta0jp8lqd/CodeImager.dmg?dl=1'
+  url 'https://www.dropbox.com/s/oe0f10c8zgd6pz2/CodeImager.dmg?dl=1'
   name 'CodeImager'
 end
