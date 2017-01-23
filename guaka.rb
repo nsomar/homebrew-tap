533228bfbeaec7f539528c2b0af779621b411fb6
Omar Abdelhafith <o.arrabi@me.com>
2017-01-23 13:48:29 +0000

    Releasing Guaka-Generator version 0.1.1

--- guaka.rb
+++ guaka.rb
@@ -1,13 +1,9 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Guaka < Formula
-  url "https://github.com/oarrabi/Guaka-Toolbox/releases/download/0.1.0/guaka"
-  version "0.1.0"
-  sha256 "ef04da9ce4d01d63c1e0d26e5b1e9c9a761af04d22329712f2709dfedda6ccaf"
+  url "https://github.com/oarrabi/Guaka-Generator/releases/download/0.1.1/guaka-generator-0.1.1-darwin-X64.tar.bz2"
+  version "0.1.1"
+  sha256 "c53641ae486c1591bca4a37c063d9d7b738ec9b5bef6fcc02027bf75ac1c59a6" 
 
-   def install
+  def install
     bin.install "guaka"
-   end
+  end
 end
