610c73f04cb4a1d865d26209119172bc30aefaca
Omar Abdelhafith <o.arrabi@me.com>
2017-01-23 13:26:51 +0000

    Releasing Guaka-Generator version 0.2.2

--- guaka.rb
+++ guaka.rb
@@ -1,7 +1,7 @@
 class Guaka < Formula
-  url "https://github.com/oarrabi/Guaka-Generator/releases/download/0.1.1/guaka-generator-0.1.1-darwin-X64.tar.bz2"
-  version "0.1.1"
-  sha256 "9a1bcf467f65a4383175cd16e33a3b594572003564961cc0723f3ebd910f17b0" 
+  url "https://github.com/oarrabi/Guaka-Generator/releases/download/0.2.2/guaka-generator-0.2.2-darwin-X64.tar.bz2"
+  version "0.2.2"
+  sha256 "" 
 
   def install
     bin.install "guaka"
